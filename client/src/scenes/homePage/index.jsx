d23f69ac98818f137df684753025a3b6c517fcd0
Ricards Taukuls <ricards.taukuls@inbox.lv>
2023-08-18 16:12:08 +0300

    Added Post widget for posting posts

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,7 +2,7 @@ import { Box, useMediaQuery } from '@mui/material'
 import { useSelector } from 'react-redux'
 import NavBar from 'scenes/navBar'
 import UserWidget from 'scenes/widgets/UserWidget'
-
+import MyPostWidget from 'scenes/widgets/MyPostWidget'
 const HomePage = () => {
 	const isNonMobileScreens = useMediaQuery('(min-width:1000px)')
 	const { _id, picturePath } = useSelector((state) => state.user)
@@ -19,7 +19,9 @@ const HomePage = () => {
 				<Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
 					<UserWidget userId={_id} picturePath={picturePath} />
 				</Box>
-				<Box flexBasis={isNonMobileScreens ? '42%' : undefined} mt={isNonMobileScreens ? undefined : '2rem'}></Box>
+				<Box flexBasis={isNonMobileScreens ? '42%' : undefined} mt={isNonMobileScreens ? undefined : '2rem'}>
+					<MyPostWidget picturePath={picturePath} />
+				</Box>
 				{isNonMobileScreens && <Box flexBasis='26%'></Box>}
 			</Box>
 		</Box>
